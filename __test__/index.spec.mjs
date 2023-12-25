77cd93af0a4a743cbd0a43589620da1a9a4e6cfd
Xitang <xz522@cornell.edu>
2023-12-25 15:33:32 -0800

    Update ava test case to pass with assertion

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -2,6 +2,7 @@ import test from 'ava';
 
 import { insertText } from '../index.js';
 
-test('Invoke insertText with empty string', () => {
+test('Invoke insertText with empty string', (t) => {
 	insertText('');
+	t.pass();
 });
